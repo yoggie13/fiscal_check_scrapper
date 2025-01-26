28ca8d104d0247257c81548bcd24a825beb2535f
Ognjen <ognjennikolic13@gmail.com>
2025-01-26 23:29:02 +0100

    api set up

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
 from ast import parse
 from flask import Flask
+from flask import request
 from flask_restful import reqparse
 from flask_cors import CORS
-import pandas as pd
 import scrapper
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +13,11 @@ CORS(app)
 
 @app.route("/scan", methods=['POST'])
 def receive_code():
-    parser = reqparse.RequestParser()
-    parser.add_argument('link', required=True, location='args')
-    scrapper.scrape_web_page(args['link'])
+    data = request.get_json()
+    print(data)
+    scrapper.scrape_web_page(data["link"])
+
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
+if __name__ == "__main__":
+    app.run(host="192.168.1.4", port="5000", debug=False, threaded = True)
